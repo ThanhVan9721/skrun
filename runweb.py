b8f6006a3987eb23d41e1c236b4a682213942ecf
Your Name <you@example.com>
2024-08-26 08:55:32 +0700

    aaaa

--- runweb.py
+++ runweb.py
@@ -1,3 +1,3 @@
 import subprocess
 
-subprocess.Popen(['tmux', 'new-session', '-d', '-s', 'websocket_session', 'python3', '/home/admin/skrun/websocket.py'])
+subprocess.Popen(['lxterminal', '--command', 'python3 /home/admin/skrun/websocket.py'])
