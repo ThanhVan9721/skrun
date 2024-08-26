7b52b3f42aa646b65b0cad8186f185169929cdd5
Your Name <you@example.com>
2024-08-26 08:28:04 +0700

    ssss

--- runweb.py
+++ runweb.py
@@ -1,7 +1,3 @@
 import subprocess
 
-# Sử dụng screen
-subprocess.Popen(['screen', '-dmS', 'websocket_session', 'python3', '/home/admin/skrun/websocket.py'])
-
-# Hoặc sử dụng tmux
-# subprocess.Popen(['tmux', 'new-session', '-d', '-s', 'websocket_session', 'python3', '/home/admin/skrun/websocket.py'])
+subprocess.Popen(['tmux', 'new-session', '-d', '-s', 'websocket_session', 'python3', '/home/admin/skrun/websocket.py'])
